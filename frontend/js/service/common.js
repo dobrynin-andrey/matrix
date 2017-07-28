a8c1268a392eb6e55271392fc758b228182c4a16
Andrey Dobrynin <andrdobrynin@gmail.com>
2017-07-28 20:42:18 +0700

    Added balloons on calculation map, changed footer and text on about page

--- frontend/js/service/common.js
+++ frontend/js/service/common.js
@@ -18,4 +18,24 @@ $(document).ready(function () {
     $('#form_delete_no').on('click', function () {
         $.fancybox.close( $('#delete-link') );
     });
+
+
+    /**
+     * Плавный скролл
+     */
+/*
+    $(".slow-scroll--js").on("click", function (event) {
+        console.log('test');
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });*/
 });
